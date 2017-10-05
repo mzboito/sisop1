d106946e809be851b46c41eb3aae64d6c47a16d8
LisianeAguiar <lisianeag@hotmail.com>
2017-10-05 16:15:54 -0300

    modificacao funcao ccreate

---
+++ cthreads/testes/cthread_tester.c
@@ -0,0 +1,22 @@
+#include ".../include/cthread.h"
+#include ".../include/cdata.h"
+#include ".../include/support.h"
+#include <stdio.h>
+#include "stdlib.h"
+#include "string.h"
+
+
+void funcaoThreadExecuta() { 
+
+printf("Thread executa");
+  
+}
+
+int main(int argc, char *argv[]) {
+
+int arg = 1;
+int teste = ccreate (funcaoThreadExecuta, &arg,0);
+return teste;
+
+
+}
