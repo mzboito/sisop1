7768ec61c83cd8add8731ca307ed45bb2258b529
mzboito <mzboito@gmail.com>
2017-10-07 17:30:40 -0300

    cyield implementada, mas nao testada. Prototipo de prioridade implementado.

--- src/cthread.c
+++ src/cthread.c
@@ -16,7 +16,8 @@ enum state {CREATED, READY, EXEC, BLOCKED, ENDED};
 TCB_t *running; //the running TCB_t pointer points to the thread running
 TCB_t main_tcb; //saves everything from the main flow
 
-int last_tid = -1;
+int last_tid = -1; //initial tid (before main)
+int cpuMz = 2294; //CPU mz rounded to the closest int value
 
 //scheduler context (necessary to know what to do when a thread finishes its job)
 ucontext_t scheduler_context;
@@ -26,6 +27,7 @@ char terminate_stack[SIGSTKSZ];
 
 
 //header for others functions in the scheduler
+void initialize();
 void terminate();
 int dispatcher();
 int dispatch(TCB_t *task);
@@ -50,27 +52,7 @@ int ccreate(void* (*start)(void*), void *arg, int prio) {
   char *stack = malloc(sizeof(char)*SIGSTKSZ); //looks more correct
 
   if(last_tid < 0){ // it means it is the first created thread from the main flow
-    last_tid = 0; //main is the pid = 0
-    //scheduler context
-    getcontext(&scheduler_context);
-    scheduler_context.uc_stack.ss_sp = scheduler_stack;
-    scheduler_context.uc_stack.ss_size = sizeof(scheduler_stack);
-    scheduler_context.uc_link = NULL; //again, not sure
-    makecontext(&scheduler_context, (void (*)(void))dispatcher, 0);
-    getcontext(&terminate_context);
-    terminate_context.uc_stack.ss_sp = terminate_stack;
-    terminate_context.uc_stack.ss_size = sizeof(terminate_stack);
-    terminate_context.uc_link = NULL;
-    makecontext(&terminate_context, (void (*)(void))terminate, 0);
-    //initialize the queues (ready, blocked and finished)
-    CreateFila2(&ready);
-    CreateFila2(&blocked);
-    CreateFila2(&finished);
-    running = malloc(sizeof(TCB_t)); //we need to separe memory area for running
-    main_tcb.tid = 0;
-    main_tcb.state = READY; //because now we are going to the thread
-    //[!!!] we are not settting priority for the main tcb
-    running = &main_tcb;
+    initialize();
     //printf("last_tid %d\n", last_tid);
     // here we need to set and save the main flow
     getcontext(&main_tcb.context);
@@ -80,14 +62,12 @@ int ccreate(void* (*start)(void*), void *arg, int prio) {
     context->uc_link = &terminate_context; //!!! not sure yet
     makecontext(context, (void(*)(void)) start, 1, arg);
   }
-
   //now we create the new TCB structure
   last_tid++;
   TCB_t *tcb = malloc(sizeof(TCB_t));
   tcb->tid = last_tid; //the first thread will be the thread 0
   tcb->state = READY;
   tcb->prio = prio;
-
   //printf("last_tid %d\n", last_tid);
   //now we need to create a new tcb-> context
   getcontext(&tcb->context);
@@ -96,14 +76,29 @@ int ccreate(void* (*start)(void*), void *arg, int prio) {
   context->uc_stack.ss_size = sizeof(char)* SIGSTKSZ;
   context->uc_link = &terminate_context;
   makecontext(context, (void (*)(void)) start, 1, arg);
-
   //printf("I entendered the ccthread, but the tid is crazy\n");
   //now we need to add it on the ready queue
   AppendFila2(&ready, (void *)tcb);
   return tcb->tid;
 }
 
-int cyield(void);
+int cyield(void){
+  if(last_tid < 0){ //there are no threads
+      initialize();
+  }
+  if(running){ //if running pointer is not null
+    int pTime = stopTimer(); //gets the number of cpu cicles the process took
+    running->state = READY;
+    int total = pTime % cpuMz;
+    running->prio = task->prio + total; //set the new priority after execution
+    AppendFila2(&ready,(void *)running);
+    printf("Inside dispatch the total is %d\n", total);
+    swapcontext(running->context, &scheduler_context);
+    return 0;
+  }
+  return -1;
+}
+
 int cjoin(int tid);
 int csem_init(csem_t *sem, int count);
 int cwait(csem_t *sem);
@@ -111,6 +106,30 @@ int csignal(csem_t *sem);
 
 
 //other functions
+void initialize(){
+  last_tid = 0; //main is the pid = 0
+  //scheduler context
+  getcontext(&scheduler_context);
+  scheduler_context.uc_stack.ss_sp = scheduler_stack;
+  scheduler_context.uc_stack.ss_size = sizeof(scheduler_stack);
+  scheduler_context.uc_link = NULL; //again, not sure
+  makecontext(&scheduler_context, (void (*)(void))dispatcher, 0);
+  getcontext(&terminate_context);
+  terminate_context.uc_stack.ss_sp = terminate_stack;
+  terminate_context.uc_stack.ss_size = sizeof(terminate_stack);
+  terminate_context.uc_link = NULL;
+  makecontext(&terminate_context, (void (*)(void))terminate, 0);
+  //initialize the queues (ready, blocked and finished)
+  CreateFila2(&ready);
+  CreateFila2(&blocked);
+  CreateFila2(&finished);
+  running = malloc(sizeof(TCB_t)); //we need to separe memory area for running
+  main_tcb.tid = 0;
+  main_tcb.state = READY; //because now we are going to the thread
+  //[!!!] we are not settting priority for the main tcb
+  running = &main_tcb;
+}
+
 void terminate(){
   running->state = ENDED;
   free(running->context.uc_stack.ss_sp); //you need to free the stack!!!
@@ -125,34 +144,20 @@ int dispatcher(){
   //InsertAfterIteratorFila2(PFILA2 pFila, void *content);
   FirstFila2(&ready);
   TCB_t *task = (TCB_t *) GetAtIteratorFila2(&ready); //get first one
-  //see if it this first one is not null (empty list)
-  if(task){
+  if(task){ //see if it this first one is not null (empty list)
     //printf("Now it will execute\n");
-    //and then remove it from ready list and then put it to run
-    DeleteAtIteratorFila2(&ready);
-    int pTime = dispatch(task); //dispatching returns how much time was spent running the task
-    printf("%d\n", pTime);
-    return pTime;
-
+    DeleteAtIteratorFila2(&ready); //and then remove it from ready list
+    startTimer();
+    dispatch(task); //dispatching returns how much time was spent running the task
   }
   printf("There are no threads left!\n\n");
-  // WHAT WE DO HERE AAAAAAAAAAAAAAAAAAAA
-  return 0;
+  exit(0);
 }
 
 int dispatch(TCB_t *task){
   task->state = EXEC;
   running = task;
-  //start the timer
   printf("Inside dispatch!\n");
-  startTimer();
   setcontext(&task->context);
-  //IT DOES NOT COMES BACK HERE
-  //stop the timer
-  int pTime = stopTimer();
-  int cpuMz = 2294;
-  int total = pTime % cpuMz;
-  printf("Inside dispatch the total is %d\n", total);
-  task->prio = task->prio + total; //set the new priority after execution
-  return total;
+  return -1; // error return
 }
