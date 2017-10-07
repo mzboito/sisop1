b819f2c5a86fd83034d683f3edf0296203181529
mzboito <mzboito@gmail.com>
2017-10-07 09:27:25 -0300

    new ccreate, compiles but does not work. -1 in the tests

--- cthreads/src/cthread.c
+++ cthreads/src/cthread.c
@@ -4,13 +4,26 @@
 #include "ucontext.h"
 #include "cdata.h"
 
+//control variables
+//a FILA2 for blockd, ready abnd finished should be added here
 
-int identificadorThread = -1;
+enum state {CREATED, READY, EXEC, BLOCKED, ENDED};
+TCB_t *running; //the running TCB_t pointer points to the thread running
+TCB_t main_tcb; //saves everything from the main flow
+
+int last_tid = -1;
+
+//scheduler context (not sure yet how to implement, but uses ucontext_t)
+
+//header for others functions in the scheduler
+int dispatcher();
+
+// main functions from cthread.h DO NOT CHANGE THE HEADER
 
 int cidentify (char *name, int size){
   int length = 18;
   const char names[length];
-    strcpy(names,"Lisiane e Marcely");
+    strcpy(names,"Lisiane\nMarcely");
     if(size < length){
       return -1;
     }else{
@@ -19,43 +32,66 @@ int cidentify (char *name, int size){
     }
 }
 
-int dispatcher ();
-
 int ccreate(void* (*start)(void*), void *arg, int prio) {
 
- char stack[SIGSTKSZ]; // pilha por enquanto
+ //char stack[SIGSTKSZ]; // pilha por enquanto
+char *stack = malloc(sizeof(char)*SIGSTKSZ); //looks more correct
 
+if(last_tid < 0){ // it means it is the first created thread from the main flow
+
+  // here we need to create the queues, the scheduler context and the structures!!
+
+  // here we need to set and save the main flow
+  getcontext(&main_tcb.context);
+  ucontext_t *context = &main_tcb.context;
+  //context->uc_link = ??
+  context->uc_stack.ss_sp = stack;
+  context->uc_stack.ss_size = sizeof(char)*SIGSTKSZ;
+  makecontext(context, (void(*)(void)) start, 1, arg);
+}
+
+//now we create the new TCB structure
+TCB_t *tcb = malloc(sizeof(TCB_t));
+tcb->tid = last_tid++; //the first thread will be the thread 0
+tcb->state = READY;
+
+//now we need to create a new tcb-> context
+getcontext(&tcb->context);
+ucontext_t *context = &tcb->context;
+//context->uc_link = ???
+context->uc_stack.ss_sp = stack;
+context->uc_stack.ss_size = sizeof(char)* SIGSTKSZ;
+makecontext(context, (void (*)(void)) start, 1, arg);
+
+//now we need to add it on the ready queue
+
+
+/* OLD VERSION
   TCB_t tcbThread = malloc (sizeof(TCB_t));
-
  ucontext_t ctx; // criar var contexto
-
  getcontext(&ctx);
-
  ctx.uc_stack.ss_sp = stack;
  ctx.uc_stack.ss_size = sizeof stack;
-
  if (identificadorThread == -1) {
-
      ctx.uc_link = &dispatcher;// pra onde vai depois da thread executar (a main aqui deu erro)
 } else {
      ctx.uc_link = &dispatcher;
 }
-
  identificadorThread++;
- tcbThread.tid = identificadorThread;
+ tcbThread.tid = last_tid;
  tcbThread.state = 0;
  tcbThread.prio = 0;
- tcbThread.context = makecontext(ctx.uc_link,start,0);
+ //tcbThread.context =
+ makecontext(ctx.uc_link,start,0);*/
 
-//inserir na fila
-
- return identificadorThread;
-
+ return tcb->tid;
 }
-
 
 int cyield(void);
 int cjoin(int tid);
 //int csem_init(csem_t *sem, int count);
 //int cwait(csem_t *sem);
 //int csignal(csem_t *sem);
+
+
+//implementacao de outras funcoes necessarias
