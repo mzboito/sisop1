9ec89f43dd44b7db2c9d8a90914398e40d51df54
lisiane <lisianeag@hotmail.com>
2017-10-12 12:16:10 -0300

    Teste para csignal

---
+++ testes/csignal_tester.c
@@ -0,0 +1,28 @@
+#include "../include/cthread.h"
+#include "../include/cdata.h"
+#include "../include/support.h"
+#include <stdio.h>
+#include "stdlib.h"
+
+void printer(csem_t *sem){
+  printf("%d\n", sem->count);
+  csignal(sem);
+  printf("TCHU TCHU PRINTING PRINTING PRINTING TCHU TCHU\n\n");
+}
+
+void foo(csem_t *sem){
+  printf("just doing my stuff here\n");
+  printer(sem);
+}
+
+
+int main(int argc, char *argv[]) {
+  csem_t *sem = malloc(sizeof(csem_t));
+  csem_init(sem,2);
+  printf("Let's create some threads, guys\n");
+  ccreate(foo, sem, 0);
+  ccreate(foo, sem, 0);
+  ccreate(foo, sem, 0);
+  cjoin(3);
+
+}
