73fdd922111774445c76036856a41f53b3fff54d
mzboito <mzboito@gmail.com>
2017-10-12 09:23:29 -0300

    csignal with bugs

--- testes/csignal_tester.c
+++ testes/csignal_tester.c
@@ -6,8 +6,11 @@
 
 void printer(csem_t *sem){
   printf("%d\n", sem->count);
-  csignal(sem);
+  cwait(sem);
   printf("TCHU TCHU PRINTING PRINTING PRINTING TCHU TCHU\n\n");
+  cyield();
+  csignal(sem);
+
 }
 
 void foo(csem_t *sem){
@@ -15,14 +18,19 @@ void foo(csem_t *sem){
   printer(sem);
 }
 
+void foo2(){
+  printf("not using the printer, leave me alone\n");
+}
+
 
 int main(int argc, char *argv[]) {
   csem_t *sem = malloc(sizeof(csem_t));
-  csem_init(sem,2);
+  csem_init(sem,1);
   printf("Let's create some threads, guys\n");
   ccreate(foo, sem, 0);
   ccreate(foo, sem, 0);
+  ccreate(foo2, sem, 0);
   ccreate(foo, sem, 0);
   cjoin(3);
-
+  printf("Now we are backing in the main!\n");
 }
